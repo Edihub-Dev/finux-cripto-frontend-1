0126f5a40615646edbea3279421e340143a4146f
Vishal Kumawat <vishalskumawat9@gmail.com>
2026-02-12 14:26:14 +0530

    code updated for make it taligram for joining us

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import './App.css'
-import LandingPage from './pages/landing/LandingPage'
+import "./App.css";
+import LandingPage from "./pages/landing/LandingPage";
+import { FaTelegramPlane } from "react-icons/fa";
 
 function App() {
-  return <LandingPage />
+  return (
+    <>
+      <LandingPage />
+      <a
+        className="telegram-fab"
+        href="https://t.me/Finuxofficiallive"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Join us on Telegram"
+      >
+        <FaTelegramPlane aria-hidden="true" />
+      </a>
+    </>
+  );
 }
 
-export default App
+export default App;
