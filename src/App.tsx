d2fb16bddb9442fd3afbbba3752b0f5f9b58222c
Vishal Kumawat <vishalskumawat9@gmail.com>
2026-02-14 16:27:07 +0530

    code updated for watsap icon and link added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import LandingPage from "./pages/landing/LandingPage";
-import { FaTelegramPlane } from "react-icons/fa";
+import { FaTelegramPlane, FaWhatsapp } from "react-icons/fa";
 
 function App() {
   return (
     <>
       <LandingPage />
+      <a
+        className="whatsapp-fab"
+        href="https://whatsapp.com/channel/0029Vb7UDolKWEKzlCe5dF30"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Join us on WhatsApp"
+      >
+        <FaWhatsapp aria-hidden="true" />
+      </a>
       <a
         className="telegram-fab"
         href="https://t.me/Finuxofficiallive"
